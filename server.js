59d6e99e22412190a2fa7f1af568e3fe47290b2d
shovancak <s.hovancak@gmail.com>
2020-09-06 15:14:03 +0200

    Creating Express application.

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Server");
+//Imports
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//Setting port for server
+const PORT = 5000;
+
+//Creating Express application
+const app = express();
+
+//Using body-parser for parsing body of incoming requests
+app.use(bodyParser.json());
+
+//Binding and listening for connections
+app.listen(PORT);
+console.log("Server running, port:", PORT);
