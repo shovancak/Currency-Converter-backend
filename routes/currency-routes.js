39cd74451ca24ec1f40fbe297d457c3e44dcd4d3
shovancak <s.hovancak@gmail.com>
2020-09-06 19:20:43 +0200

    Using express-validator for valdiating data in request body.

--- routes/currency-routes.js
+++ routes/currency-routes.js
@@ -1,5 +1,6 @@
 //Imports
 const express = require("express");
+const { check } = require("express-validator");
 const currencyControllers = require("../controllers/currency-controllers");
 
 //Using Express Router
@@ -7,7 +8,11 @@ const router = express.Router();
 
 //Routes
 router.get("/currency-data", currencyControllers.getLatestCurrencyData);
-router.post("/conversion", currencyControllers.currencyConversion);
+router.post(
+  "/conversion",
+  [check("amount").exists({ checkNull: true, checkFalsy: true }).isNumeric()], // => Validating if data in request body exists and are numeric
+  currencyControllers.currencyConversion
+);
 
 //Exports
 module.exports = router;
