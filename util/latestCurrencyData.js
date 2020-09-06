f14f85504fe858cc91178886dfd62d28686c7a9a
shovancak <s.hovancak@gmail.com>
2020-09-06 16:12:40 +0200

    Creating function for getting latest currency data.

---
+++ util/latestCurrencyData.js
@@ -0,0 +1,26 @@
+//Imports
+const axios = require("axios");
+
+// APP_ID / API KEY used for accessing the Open Exchange Rates API
+const APP_ID = "6f322c73837e425b9894333e4bb4ffb9";
+
+// Function for getting latest currency data (name, rate) from external API
+const latestCurrencyData = async () => {
+  try {
+    const response = await axios.get(
+      `https://openexchangerates.org/api/latest.json?app_id=${APP_ID}`
+    );
+    const latestData = await response.data.rates;
+    return latestData;
+  } catch (err) {
+    //Error handling
+    const error = new Error(
+      "Something went wrong, could not get latest currency data."
+    );
+    error.code = 404;
+    throw error;
+  }
+};
+
+//Exports
+module.exports = latestCurrencyData;
