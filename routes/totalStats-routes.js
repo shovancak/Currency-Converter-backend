945b8e82d506560ca7400a0d4639ff3ca635e0df
shovancak <s.hovancak@gmail.com>
2020-09-08 14:14:36 +0200

    Using express-validator for valdiating data in request body.

--- routes/totalStats-routes.js
+++ routes/totalStats-routes.js
@@ -1,12 +1,23 @@
 //Imports
 const express = require("express");
+const { check } = require("express-validator");
 const totalStatsControllers = require("../controllers/totalStats-controller");
 
 //Using Express Router
 const router = express.Router();
 
 //Routes
-router.patch("/", totalStatsControllers.updateTotalStats);
+router.patch(
+  "/",
+  [
+    check("amount")
+      .exists({ checkNull: true, checkFalsy: true })
+      .isNumeric()
+      .not()
+      .isString(),
+  ], // => Validating if amount in request body is numeric
+  totalStatsControllers.updateTotalStats
+);
 
 //Exports
 module.exports = router;
