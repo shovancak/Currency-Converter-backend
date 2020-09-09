a5570380e019191077f72182582ff61c04698551
shovancak <s.hovancak@gmail.com>
2020-09-09 13:11:57 +0200

    Adding logic for finding most popular currency.

--- controllers/totalStats-controller.js
+++ controllers/totalStats-controller.js
@@ -59,6 +59,24 @@ const updateTotalStats = async (req, res, next) => {
     stats.conversionsOfCurrency.push({ name: currency, conversions: 1 });
   }
 
+  //Finding most popuplar currency => currency with highest amount of conversions made
+  // Storing array of all currencies used
+  const arrayOfConvertedCurrencies = stats.conversionsOfCurrency;
+  //Sorting array in descending order => from highest to loves value of conversions
+  const sortedArray = arrayOfConvertedCurrencies.sort(function (a, b) {
+    return b.conversions - a.conversions;
+  });
+  //Checking if there is currency with highest amount of conversions made
+  //If amount of conversion of first currency is equal to amount of conversions second currency
+  // mostPopularCurrency is set to "", it means there is no ONE most popular currency
+  if (sortedArray[0].conversions === sortedArray[1].conversions) {
+    stats.mostPopularCurrency = "";
+  } else {
+    //=> Else most popular currency is set to first element in array (element with highest aount of conversions)
+    const mostPopularCurrency = sortedArray[0].name;
+    stats.mostPopularCurrency = mostPopularCurrency;
+  }
+
   //Saving updated values in database
   try {
     await stats.save();
